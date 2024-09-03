6517bf1482693b6bfe0693693f39804bcfa731d8
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-04 00:25:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 //your JS code here. If required.
-
 function enterBtn() {
+    // Get the paragraph element with the id "status"
+    const para = document.getElementById("status");
     
-  const para=document.getElementById("status")
-
-const head=document.createElement("h1")
- head.innerText="Entered Metaverse"
- para.parentNode.replaceChild(head, para);
+    // Create a new <h1> element
+    const head = document.createElement("h1");
+    
+    // Set the text content of the new <h1> element
+    head.innerText = "Entered Metaverse";
+    
+    // Replace the <p> element with the new <h1> element
+    para.parentNode.replaceChild(head, para);
 }
