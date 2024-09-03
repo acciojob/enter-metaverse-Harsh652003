f2f632dba106f357ddee5d12f5ff0a2e6b7fb7bc
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-04 00:06:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+function enterBtn() {
+    
+  const para=document.getElementById("status")
+
+const head=document.createElement("h1")
+ head.innerText="Entered Metaverse"
+ para.parentNode.replaceChild(head, para);
+}
