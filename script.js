e2987ba97f8dac70e97d5b7371f3d7d4ce9eb0a9
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-05 00:02:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ function enterBtn() {
     
     // Set the text content of the new <h1> element
     head.innerText = "Entered Metaverse";
-    head.id="status"
-    // Replace the <p> element with the new <h1> element
-    para.parentNode.replaceChild(head, para);
+    
+    // Clear the content of the <p> element
+    para.innerHTML = '';
+    
+    // Append the new <h1> element to the <p> element
+    para.appendChild(head);
 }
